c3fc8c6e2aafdfd2f41c50b4b67735683b25dcc6
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-10-03 08:56:05 -0600

    Added comments, and created the ArrayList.

--- src/list/controller/ListController.java
+++ src/list/controller/ListController.java
@@ -2,11 +2,20 @@ package list.controller;
 
 import java.util.List;
 import java.util.ArrayList;
+import list.model.Donut;
+
 public class ListController 
 {
+	private List<Donut> donutList;
+	
+	public ListController()
+	{
+		donutList = new ArrayList<Donut>();
+	}
 	
 	public void start()
 	{
-		
+		System.out.println(donutList);
+		donutList.add(new Donut());
 	}
 }
